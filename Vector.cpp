5daf37d6021d4e2242111da7c7c64c272d6e9340
AnthonyMikh <hedgehog1024@yandex.ru>
2016-05-14 19:01:36 +0300

    Add files via upload
    Added the very basic functions

---
+++ Vector.cpp
@@ -0,0 +1,75 @@
+template <class T> class Vector
+{
+    typedef T  value_type;
+    typedef T* iterator;
+    typedef const T* const_iterator;
+    typedef unsigned size_type;
+
+    private:
+        size_type elemcount,space;
+        T* arr;
+        const static size_type memory_chunk = 8;
+
+    public:
+        //constructors and destructor
+        Vector():elemcount(0),space(0),arr(new T[0]){}
+       ~Vector() { delete [] arr;}
+        explicit Vector (size_type nelems): elemcount(nelems), space(nelems)
+        {
+            arr = new T[nelems];
+            for (size_type i = 0; i < nelems; ++i)
+                arr[i] = 0;
+        }
+
+        //returning basic properties
+        size_type size()
+        {
+            return elemcount;
+        }
+        size_type capacity()
+        {
+            return space;
+        }
+
+        //memory manipulation
+        void reserve (size_type newspace)
+        {
+            if (newspace <= space)
+                return;
+            T* newarr = new T[newspace];
+            for (size_type i = 0; i < newspace; ++i)
+                newarr[i] = arr[i];
+            arr = newarr;
+            space = newspace;
+        }
+        void resize (newsize)
+        {
+            if (newsize == elemcount)
+                return;
+            T* newarr = new T[newsize];
+            delete [] arr;
+            arr = newarr;
+            elemcount = newsize;
+            space = newsize;
+        }
+        void push_back (T& newvalue)
+        {
+            if (space == elemcount)
+                reserve(space*2);
+            if (space == 0)
+                reserve(memory_chunk);
+            arr[elemcount] = newvalue;
+            ++elemcount;
+        }
+
+        //operators
+        T& operator[](size_type i)
+        {
+            return arr[i];
+        }
+        const T& operator[](size_type i) const
+        {
+            return arr[i];
+        }
+
+};
